5a6d49e44375d4fea4eb83eb32293e4aa4e63dfe
Forest Tong <ftong@mit.edu>
2015-04-26 08:24:13 +0200

    message by filtering from pool

--- TextbookApp/Chats/ChatsViewController.swift
+++ TextbookApp/Chats/ChatsViewController.swift
@@ -14,22 +14,25 @@ class ChatsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let chat = Chat(me: PFUser.currentUser()!, you: PFUser.currentUser()!, lastMessageSentDate: NSDate())
-        chat.save()
-        let i = PFUser.currentUser()!
-        chat.messages = [
-//            Message(me: i, you: i, text: "I really enjoyed programming with you! :-)", sentDate: NSDate(timeIntervalSinceNow: -60*60*24*2-60*60)),
-//            Message(me: i, you: i, text: "Thanks! Me too! :-)", sentDate: NSDate(timeIntervalSinceNow: -60*60*24*2)),
-//            Message(me: i, you: i, text: "Hey, would you like to spend some time together tonight and work on Acani?", sentDate: NSDate(timeIntervalSinceNow: -33)),
-//            Message(me: i, you: i, text: "Sure, I'd love to. How's 6 PM?", sentDate: NSDate(timeIntervalSinceNow: -19)),
-//            Message(me: i, you: i, text: "6 sounds good :-)", sentDate: NSDate())
-        ]
-        let tempChats = [chat]
-        let currentUser = PFUser.currentUser()!
-        currentUser.setObject(tempChats, forKey: "chats")
-        currentUser.saveInBackgroundWithBlock(nil)
-        chats = PFUser.currentUser()!.objectForKey("chats") as! [Chat]
-        println(chats[0])
+//        let chat = Chat(me: PFUser.currentUser()!, you: PFUser.currentUser()!, lastMessageSentDate: NSDate())
+//        chat.save()
+//        let i = PFUser.currentUser()!
+//        chat.messages = [
+////            Message(me: i, you: i, text: "I really enjoyed programming with you! :-)", sentDate: NSDate(timeIntervalSinceNow: -60*60*24*2-60*60)),
+////            Message(me: i, you: i, text: "Thanks! Me too! :-)", sentDate: NSDate(timeIntervalSinceNow: -60*60*24*2)),
+////            Message(me: i, you: i, text: "Hey, would you like to spend some time together tonight and work on Acani?", sentDate: NSDate(timeIntervalSinceNow: -33)),
+////            Message(me: i, you: i, text: "Sure, I'd love to. How's 6 PM?", sentDate: NSDate(timeIntervalSinceNow: -19)),
+////            Message(me: i, you: i, text: "6 sounds good :-)", sentDate: NSDate())
+//        ]
+//        let tempChats = [chat]
+//        let currentUser = PFUser.currentUser()!
+//        currentUser.setObject(tempChats, forKey: "chats")
+//        currentUser.saveInBackgroundWithBlock(nil)
+//        chats = PFUser.currentUser()!.objectForKey("chats") as! [Chat]
+//        println(chats[0])
+        chats = []
+        
+        
         
         tableView.backgroundColor = UIColor.whiteColor()
         tableView.rowHeight = chatCellHeight
@@ -38,23 +41,23 @@ class ChatsViewController: UITableViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-//        fetchChats()
-//        tableView.reloadData()
+        fetchChats()
+        tableView.reloadData()
     }
     
-//    func fetchChats() {
-//        let query = Chat.query()!
-//        query.includeKey("you")
-//        query.includeKey("me")
-//        query.includeKey("messages")
-//        query.whereKey("me", equalTo: PFUser.currentUser()!)
-//        query.findObjectsInBackgroundWithBlock { objects, error -> Void in
-//            if let myChats = objects as? [Chat] {
-//                self.chats = myChats
-//                self.tableView.reloadData()
-//            }
-//        }
-//    }
+    func fetchChats() {
+        let query = Chat.query()!
+        query.includeKey("you")
+        query.includeKey("me")
+        query.includeKey("messages")
+        query.whereKey("me", equalTo: PFUser.currentUser()!)
+        query.findObjectsInBackgroundWithBlock { objects, error -> Void in
+            if let myChats = objects as? [Chat] {
+                self.chats = myChats
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return chats.count
